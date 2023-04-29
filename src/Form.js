d76e3db01d376a2ae99a10172186dc7467add93f
unknown <olokedeoladotun@gmail.com>
2023-04-29 02:02:30 +0100

    checking

--- src/Form.js
+++ src/Form.js
@@ -4,18 +4,18 @@ function Form() {
   return (
     <div>
         <form>
-            {/* <div id='form-div'> */}
+            <div id='form-div'>
             <input placeholder='Email Address or Phone number' ></input> <br></br>
-            <input placeholder='Password' ></input> <br></br>
+            <input  placeholder='Password'  type='password'></input> <br></br>
             <button id='btt-login'>Log in</button> <br></br>
             <a href='#'>forgotten Password? </a> <br></br>
              <hr></hr> <br></br>
             <button id='btt-newaccount'> Creat new account</button> <br></br>
            
-            {/* </div> */}
+            </div>
         
         </form>
-        <div id='last-para'>Create a Page for a celebrity, brand or business.</div>
+        <div id='last-para'> <b>Create a Page</b> for a celebrity, brand or business.</div>
     </div>
   )
 }
